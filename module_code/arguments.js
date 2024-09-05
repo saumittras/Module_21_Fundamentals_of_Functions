824f6aae4b4eb29ec0dc4e9d1f2134a560a20cf6
saumittras <saumittra.eee@gmail.com>
2024-09-05 22:35:26 +0600

    added 2nd and 3dr video code;

---
+++ module_code/arguments.js
@@ -0,0 +1,17 @@
+function doubleIt(number) {
+  const doubled = number * 2;
+  console.log(number, doubled);
+}
+
+doubleIt(45);
+
+const number = 55;
+doubleIt(number);
+function difference(num1, num2) {
+  const diff = num1 - num2;
+  console.log(num1, num2, "dufference is", diff);
+}
+
+const fage = 40;
+const myage = 10;
+difference(fage, myage);
---
+++ module_code/parameters.js
@@ -0,0 +1,11 @@
+function squre(number) {
+  const result = number ** 2;
+  return result;
+}
+
+console.log(squre(5));
+function add(num1, num2) {
+  const sum = num1 + num2;
+  console.log(sum);
+}
+add(45, 5);
