1aaa0b6f1c139172562f1fc2e92fb131684a60bb
saumittras <saumittra.eee@gmail.com>
2024-09-05 20:51:42 +0600

    decleare a function

--- module_code/function.js
+++ module_code/function.js
@@ -1,1 +1,16 @@
-hi;
+const number = 45;
+
+function fanoffkor() {
+  console.log("Bosa the uthe dara");
+  console.log("walk towards the switch");
+  console.log("click the switch to off the Fan");
+}
+
+function brushTeeth() {
+  console.log("pick up the brush");
+  console.log("add past ");
+  console.log("2 min goso and goso and fena felo");
+}
+
+brushTeeth();
+fanoffkor();
