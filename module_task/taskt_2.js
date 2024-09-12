e40ed2033ecc5c04df6613e9da5b57e745c20599
saumittras <saumittra.eee@gmail.com>
2024-09-13 00:36:41 +0600

    added module task 1

--- module_task/taskt_2.js
+++ module_task/taskt_2.js
@@ -3,3 +3,13 @@
  * Task-2
 Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
  */
+function numberReturn(num) {
+  if (num % 2 === 0) {
+    return num / 2;
+  } else if (num % 2 === 1) {
+    return num * 2;
+  } else {
+    return "Invalid Input";
+  }
+}
+console.log(numberReturn("101a"));
