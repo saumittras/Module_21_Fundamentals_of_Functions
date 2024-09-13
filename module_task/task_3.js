b92d825827b72b2f0df98f84021e61a14363d84c
saumittras <saumittra.eee@gmail.com>
2024-09-13 09:59:59 +0600

    added module task 3

--- module_task/task_3.js
+++ module_task/task_3.js
@@ -5,3 +5,14 @@ Write a function called make_avg() which will take an array of integers and the 
 
 https://github.com/ProgrammingHero1/js-function-practice-tasks
  */
+
+function average(arr) {
+  let sum = 0;
+  for (const num of arr) {
+    sum += num;
+  }
+  return sum / arr.length;
+}
+
+const arr = [4, 5, 6, 7, 11, 23, 45, 65];
+console.log(average(arr));
