524175cfdee690ae7dde13a09a0bb0cb2264328b
saumittras <saumittra.eee@gmail.com>
2024-09-13 10:02:53 +0600

    added module task

--- module_task/task_1.js
+++ module_task/task_1.js
@@ -3,3 +3,8 @@
  Task-1
 Take four parameters. Multiply the four numbers and then return the result
  */
+function multiplayFore(num1, num2, num3, num4) {
+  return num1 * num2 * num3 * num4;
+}
+
+console.log(multiplayFore(3, 4, 5, 6));
